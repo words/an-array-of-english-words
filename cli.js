fb3eabb62fe89e7a54c3e099b9ab0c7a6847bcf7
Max Bigras <mbigras22@gmail.com>
2017-01-30 14:38:58 -0800

    :bug: Fix EPIPE error when piping into head
    https://github.com/nodejs/node-v0.x-archive/issues/3211
    https://github.com/mhart/epipebomb

--- cli.js
+++ cli.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 // Writes a grepable list of English words to stdout
 
+require('epipebomb')()
+
 words = require('./words.js')
 process.stdout.write(words().join('\n') + "\n")
+
+
+
+
