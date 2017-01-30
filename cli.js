c0b5302546504c3e8d73739e7872c91620afb770
Max Bigras <mbigras22@gmail.com>
2017-01-30 15:10:00 -0800

    Add cli

--- cli.js
+++ cli.js
@@ -2,10 +2,32 @@
 // Writes a grepable list of English words to stdout
 
 require('epipebomb')()
+var words = require('./words.js'),
+    argv = require('minimist')(process.argv.slice(2));
 
-words = require('./words.js')
-process.stdout.write(words().join('\n') + "\n")
 
+main = (argv) => {
+  if (argv.help || argv.h)
+    process.stdout.write(help)
+  else if (argv.version || argv.v)
+    process.stdout.write(version + "\n")
+  else {
+    var re = new RegExp(argv._.shift())
+    process.stdout.write(words({re: re}).join('\n') + "\n")
+  } 
+}
+
+var version = require('./package.json').version
+var help = `Usage: words [options] RE
+
+v${version}
+
+Options:
+  -h, --help      Show command line help
+  -v, --version   Show version info
+`
+
+main(argv)
 
 
 
