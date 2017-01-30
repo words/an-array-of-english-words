d995cae4fbfc1d730232062e018249eccc9cd0e4
Max Bigras <mbigras22@gmail.com>
2017-01-30 14:19:15 -0800

    Change build.js to export build

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
 const expect = require('chai').expect
-const words = require('.')
+const build = require('./build.js')()
 const they = it
 
-describe('words', () => {
+describe('build', () => {
   they('are in an array', () => {
-    expect(words).to.be.an('array')
+    expect(build).to.be.an('array')
   })
 
   they('are over 274,000 in number', () => {
-    expect(words.length).to.be.above(274 * 1000)
+    expect(build.length).to.be.above(274 * 1000)
   })
 
   they('can have one letter', () => {
-    expect(words.filter(word => word.length === 1).length).to.be.above(4)
+    expect(build.filter(word => word.length === 1).length).to.be.above(4)
   })
 
   they('can have two letters', () => {
-    expect(words.filter(word => word.length === 2).length).to.be.above(50)
+    expect(build.filter(word => word.length === 2).length).to.be.above(50)
   })
 
   they('are all lowercase', () => {
-    expect(words.every(word => word === word.toLowerCase())).to.equal(true)
+    expect(build.every(word => word === word.toLowerCase())).to.equal(true)
   })
 
   they('all have length', () => {
-    expect(words.every(word => word.length > 0)).to.equal(true)
+    expect(build.every(word => word.length > 0)).to.equal(true)
   })
 })
