8269ce44f183707f7e5f4035a41a808a76f8405d
Max Bigras <mbigras22@gmail.com>
2017-01-30 16:44:54 -0800

    Add bold exe name in help

--- cli.js
+++ cli.js
@@ -2,23 +2,24 @@
 // Writes a grepable list of English words to stdout
 
 require('epipebomb')()
-var words = require('./words.js'),
-    argv = require('minimist')(process.argv.slice(2));
+const chalk = require('chalk'),
+      words = require('./words.js');
+var argv = require('minimist')(process.argv.slice(2));
 
 
 main = (argv) => {
   if (argv.help || argv.h)
-    process.stdout.write(help)
+    process.stdout.write(help);
   else if (argv.version || argv.v)
-    process.stdout.write(version + "\n")
+    process.stdout.write(version + "\n");
   else {
-    var re = new RegExp(argv._.shift())
-    process.stdout.write(words({re: re}).join('\n') + "\n")
+    var re = new RegExp(argv._.shift());
+    process.stdout.write(words({re: re}).join('\n') + "\n");
   } 
 }
 
-var version = require('./package.json').version
-var help = `Usage: words [options] [pattern]
+var version = require('./package.json').version,
+    help = `Usage: ${chalk.bold('words')} [options] [pattern]
 
 v${version}
 
@@ -27,14 +28,14 @@ Options:
   -v, --version   Show version info
 
 Examples:
-words '^foobar$'
+${chalk.bold('words')} '^foobar$'
 foobar
-words '^f..bar$'
+${chalk.bold('words')} '^f..bar$'
 foobar
 
 Arguments:
   pattern
       Regular expression to search for
-`
+`;
 
-main(argv)
+main(argv);
